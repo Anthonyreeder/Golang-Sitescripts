8e51f5bbaa7c1a671336ad91322c624cda6af5e9
Anthonyreeder <anthonyreeder123@gmail.com>
2021-10-07 13:00:43 +0100

    Started adding new project
    Added the global file to store URL (and probably more later) for easy accessible global data.

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	global "./model"
+)
+
+func main() {
+	//This would otherwise come from the UI. Hardcoding for development purposes.
+	global.Url = ""
+}
---
+++ model/global.go
@@ -0,0 +1,4 @@
+package model
+
+var StartTime = ""
+var Url = ""
